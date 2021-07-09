744660792d119a587286e142a73d9c884c68eba4
meroa006 <merouani.ass@gmail.com>
2021-07-09 11:13:07 -0400

    Initial commit

---
+++ src/main/java/ucf/assignments/ToDoListController.java
@@ -0,0 +1,191 @@
+package ucf.assignments;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Asad merouani
+ */
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ToDoListController implements Initializable {
+    TodolistTableManager todolistTableManager = new TodolistTableManager();
+    ObservableList<TaskItem> todoItemsData;
+    ObservableList<ToDoTask> taskDataList;
+    ObservableList<TaskItem> todoItemsDataIncomplete;
+    ObservableList<TaskItem> todoItemsDataComplete;
+    @FXML
+    private TableView<ToDoTask> toDoTaskTableView;
+
+    @FXML
+    private TableView<TaskItem> taskItemTableView;
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private MenuItem closeMenu;
+
+    @FXML
+    private MenuItem newMenu;
+
+    @FXML
+    private MenuItem openMenu;
+
+    @FXML
+    private MenuItem showCompleteItemsMenu;
+
+    @FXML
+    private MenuItem showIncompleteItemsMenu;
+
+    @FXML
+    private MenuItem saveTaskMenu;
+
+    @FXML
+    private MenuItem saveItemsMenu;
+
+    @FXML
+    private TableColumn toDoColumn;
+
+    @FXML
+    private TableColumn Items;
+
+    @FXML
+    private TableColumn descriptionColumn;
+
+    @FXML
+    private TableColumn dueDateColumn;
+
+    @FXML
+    private TextField addToDoListText;
+
+    @FXML
+    private Button AddTaskButton;
+
+    @FXML
+    private TextField itemName;
+
+    @FXML
+    private TextField description;
+
+    @FXML
+    private Button addItem;
+
+    @FXML
+    private TextField dueDate;
+
+    @FXML
+    void AddTaskButtonClicked(ActionEvent event) {
+
+        // get the the textField content
+        // call add method of TodolistManager to set the the new content into the todolist.
+        // clear the textField
+        //final ObservableList<ToDoTask> data = TasksTableView.getItems();
+
+    }
+
+    @FXML
+    public void addItemClicked(ActionEvent actionEvent) {
+
+        // get the the textField content
+        // call TodolistManager to set the the new content into the todolist.
+        // clear the textField
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // populates the tableView// has the capacity to store at lease 100 tasks.
+        // toDoTaskTableView.getItems();
+        // populates the tableView for items// has the capacity to store at lease 100 items.
+        // Set the cells todoTasks  table to be editable
+        // Call setEditable function to make the title editable.
+        // call setEditable function to make description and and due date editable
+
+    }
+
+    @FXML
+    void saveItemsMenuClicked(ActionEvent event) {
+
+        // get The variable of the Observable Collection data of the list
+        // get the selected cell from observable collection
+        // create object of the todoTask
+        // call remove function of the observable collection
+
+    }
+
+    // Load multiple application with different data files
+    @FXML
+    void newMenuClicked(ActionEvent event) {
+        // the method new of the todolistTableManager
+
+
+    }
+
+    @FXML
+    void setStatusCompleteClicked(ActionEvent event) {
+        // call changeStatus method of the todolistTableManager
+    }
+
+    @FXML
+    void setStatusIncompleteClicked(ActionEvent event) {
+        // call changeStatus method of the todolistTableManager
+        todolistTableManager.changeStatus("incomplate");
+    }
+    // Load existing data files
+    @FXML
+    void openMenuClicked(ActionEvent event) {
+        // the method open of the todolistTableManager
+
+    }
+
+
+    @FXML
+    void closeMenuClicked(ActionEvent event) {
+        // the method close of the todolistTableManager
+
+
+    }
+
+    @FXML
+    void showCompleteMenuClicked(ActionEvent event) {
+
+        // call show showIncompleteItems method of the todolistTableManager.
+
+    }
+
+    @FXML
+    void showIncompleteMenuClicked(ActionEvent event) {
+
+        // call show showIncompleteItems method of the todolistTableManager.
+    }
+    @FXML
+    void showAllItemsMenuClicked(ActionEvent event) {
+        // call show showAllItems method of the todolistTableManager.
+        todolistTableManager.showAllItems();
+    }
+    @FXML
+    void saveTaskMenuClicked(ActionEvent event) {
+
+        // Get the Observable data collection of the task list
+        // call the save function in the ToDoListManager
+    }
+
+    @FXML
+    public void deleteTaskClicked(ActionEvent actionEvent) {
+        // call the method delete of the todolistTaskManager
+
+    }
+
+    @FXML
+    public void deleteItemClicked(ActionEvent actionEvent) {
+        // call the method delete of the todolistTaskManager
+
+    }
+
+}
---
+++ src/main/java/ucf/assignments/TodolistTableManager.java
@@ -0,0 +1,110 @@
+package ucf.assignments;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Asad merouani
+ */
+
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+
+public class TodolistTableManager {
+    ToDoListApp application = new ToDoListApp();
+
+    public void add(String text, TableColumn<String, String> toDoListTable) {
+        // get the column of the tableView
+        // add the new value to the column the tableview by calling the get items methods of the Observable Collection
+
+
+    }
+    public void sort(TableView table, TableColumn column){
+        // call the built in sort method of the Observable Object.
+
+    }
+
+    public void save(ObservableList<ToDoTask> taskDataList) {
+        // open json file
+        // the data of the tableView
+        // Do serialization of the object to json Object
+        // write the data in the file
+        // close the file
+
+    }
+
+
+    public void setTableEditable(TableColumn toDoColumn, String columnTittle) {
+
+        // the call of this method will enable the user to edit title of the task
+        // + the description of an item and dueDate
+
+        // call SetCellFactory method to make the cell as text field
+        // call setOnEditCommit and assign click event to the column
+        // set The object of the type ToDoTask with the new entered value
+        // add the new value to the table view data
+        // refresh the Tableview rendering
+    }
+
+    public void delete(TableView<ToDoTask> toDoTaskTableView) {
+        // Get Observable Collection data of the list
+        // get the selected cell from observable collection
+        // create object of the todoTask
+        // call remove function of the observable collection
+    }
+
+    public void showIncompleteItems(TableView<TaskItem> taskItemTableView) {
+        // Get Observable Collection data of the list.
+        // Create an object of the class ToDoTask.
+        // Set the object status to complete.
+        // Get the TableView of the item.
+        // Get the status column.
+        // Get all the elements of the Observable collection that has a incomplete status.
+        // Assign the incomplete items to the New observable collection data.
+        // Load the new observable collection data to the TableView To display.
+    }
+
+    public void showCompleteItems(TableView<TaskItem> taskItemTableView) {
+        // Get Observable Collection data of the list.
+        // Create an object of the class ToDoTask.
+        // Set the object status to complete.
+        // Get the TableView of the item.
+        // Get the status column.
+        // Get all the elements of the Observable collection that has a complete status.
+        // Assign the completed items to the New observable collection data.
+        // Load the new observable collection data to the TableView To display.
+    }
+
+    public void newApp() {
+        // this allows the user to load multiple file in different application.
+        // creates object from the TodoListApp.
+        // build new stage
+        // call start method of the TodoListApp object
+    }
+
+    public void open() {
+        // open windows dialogue
+        // enable the user navigate in folders
+        // enable the user to select the file
+        // open the Json data file
+        // load the new data to the interface
+
+    }
+
+    public void close() {
+        // get the instance of the current application from main
+        // terminate the platform application
+
+    }
+
+    public void changeStatus(String status) {
+        // get the items Tableview
+        // get the status row
+        // set the textField of the cell to the message in status
+
+    }
+
+    public void showAllItems() {
+        // Get Observable Collection data of the list.
+        // Load the  observable collection data to the TableView To display.
+    }
+}
